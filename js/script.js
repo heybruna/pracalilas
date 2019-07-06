e7121dc464dd638312381c9900a05d0b274f157a
Bruna <bcrussi@gmail.com>
2019-07-06 14:30:36 -0300

    Website 1.0

---
+++ js/script.js
@@ -0,0 +1,44 @@
+function menuGoTo() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+}
+
+function scrollToTop() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+}
+
+$(document).ready(function() {
+    menuGoTo();
+    scrollToTop();
+
+    $('#scroll').click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+
+});
